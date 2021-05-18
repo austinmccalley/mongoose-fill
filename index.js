65697e82c36bf15e25cb70a224a9c4c599a38c60
Austin McCalley <austin@austinmccalley.com>
2021-05-17 21:16:35 -0700

    Updated deprecated features and format

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var async = require('async')
 var util = require('util')
 
-var getArgsWithOptions = function(__fill){
+var getArgsWithOptions = function (__fill) {
   var args = [],
     options = __fill.fill.options
 
@@ -12,14 +12,14 @@ var getArgsWithOptions = function(__fill){
     ? __fill.opts
     : options
 
-  if (opts && opts.length){
+  if (opts && opts.length) {
 
     // add lacking or remove excessive options
-    if (options){
+    if (options) {
       var diff = opts.length - options.length
-      if (diff < 0){
+      if (diff < 0) {
         opts = opts.concat(options.slice(diff))
-      } else if (diff > 0){
+      } else if (diff > 0) {
         opts = opts.slice(0, diff)
       }
     }
@@ -28,11 +28,11 @@ var getArgsWithOptions = function(__fill){
   return args
 }
 
-var fillDoc = function(doc, __fill, cb){
+var fillDoc = function (doc, __fill, cb) {
 
   var args = getArgsWithOptions(__fill)
 
-  if (__fill.fill.fill){
+  if (__fill.fill.fill) {
     args.unshift(doc)
     args.push(cb)
     __fill.fill.fill.apply(__fill.fill, args)
@@ -43,13 +43,13 @@ var fillDoc = function(doc, __fill, cb){
     var multipleProps = __fill.fill.props.length > 1
 
     //args.unshift(doc)
-    args.push(function(err, val){
+    args.push(function (err, val) {
       // if val is not passed, just leave it
-      if (arguments.length > 1 && val !== doc){
-        if (prop){
+      if (arguments.length > 1 && val !== doc) {
+        if (prop) {
           doc.set(prop, multipleProps ? val[prop] : val)
         } else {
-          props.forEach(function(prop){
+          props.forEach(function (prop) {
             doc.set(prop, val[prop])
           })
         }
@@ -63,43 +63,43 @@ var fillDoc = function(doc, __fill, cb){
   }
 }
 
-var checkAlreadyHasFill = function(__fillsSequence, fill){
+var checkAlreadyHasFill = function (__fillsSequence, fill) {
   return __fillsSequence.reduce((has, __fills) =>
     has || __fills
       .filter(__fill => __fill.fill === fill)[0]
     , false)
 }
 
-var checkAlreadyHasFillProp = function(__fillsSequence, prop){
+var checkAlreadyHasFillProp = function (__fillsSequence, prop) {
   return __fillsSequence.reduce((has, __fills) =>
     has || __fills
       .filter(__fill => __fill.fill.prop === prop).length > 0
-  , false)
+    , false)
 }
 
-var addFills = function(__fillsSequence, Model, props, opts, unshift) {
+var addFills = function (__fillsSequence, Model, props, opts, unshift) {
   let __fills = []
   props = (typeof props == 'string') ? props.split(' ') : props
   props.forEach((prop) => {
     prop = prop.trim()
     var fill = Model.__fill && Model.__fill[prop]
-    if (fill){
+    if (fill) {
       var __fill = checkAlreadyHasFill(__fillsSequence, fill)
-      if (__fill){
-        if (__fill.props.indexOf(prop) < 0){
+      if (__fill) {
+        if (__fill.props.indexOf(prop) < 0) {
           __fill.props.push(prop)
         }
       } else {
-        __fills.push({fill: fill, opts: opts, props: [prop]})
+        __fills.push({ fill: fill, opts: opts, props: [prop] })
       }
     } else {
       var propSplit = prop.split('.').filter(_ => _)
-      if (propSplit.length > 1){
-        if (checkAlreadyHasFillProp(__fillsSequence, prop)){
+      if (propSplit.length > 1) {
+        if (checkAlreadyHasFillProp(__fillsSequence, prop)) {
           return
         }
         let propToFill = propSplit.slice(0, -1).join('.')
-        
+
         if (propToFill.indexOf('.') > 0 || (Model.__fill && Model.__fill[propToFill])) {
           addFills(
             __fillsSequence, Model,
@@ -109,13 +109,13 @@ var addFills = function(__fillsSequence, Model, props, opts, unshift) {
         var fieldProp = propSplit.shift()
         fill = {
           prop: prop,
-          fill: function ()  {
+          fill: function () {
             var opts = Array.prototype.slice.call(arguments);
             var doc = opts.shift()
             var callback = opts.pop()
-            if (doc[fieldProp]){
+            if (doc[fieldProp]) {
               var docs = doc[fieldProp]
-              if (!Array.isArray(docs)){
+              if (!Array.isArray(docs)) {
                 docs = [docs]
               }
               async.map(docs, (doc, cb) => {
@@ -132,7 +132,7 @@ var addFills = function(__fillsSequence, Model, props, opts, unshift) {
             }
           }
         }
-        __fills.push({fill: fill, opts: opts, props: [prop]})
+        __fills.push({ fill: fill, opts: opts, props: [prop] })
       } else {
         throw new Error('fill for property "' + prop + '" not found')
       }
@@ -140,7 +140,7 @@ var addFills = function(__fillsSequence, Model, props, opts, unshift) {
     fill.db = Model.db
 
   })
-  if (__fills.length){
+  if (__fills.length) {
     unshift
       ? __fillsSequence.unshift(__fills)
       : __fillsSequence.push(__fills)
@@ -154,7 +154,7 @@ var getPromise = function (cb) {
   if (global.Promise || mongoose.PromiseProvider) {
     var Promise = global.Promise || mongoose.PromiseProvider.get().ES6
     promise = new Promise((_resolve, _reject) => {
-      onResolve = () => {}
+      onResolve = () => { }
       resolve = (err, res) => {
         err ? _reject(err) : _resolve(res)
         cb && cb(err, res)
@@ -166,8 +166,8 @@ var getPromise = function (cb) {
     resolve = promise.resolve.bind(promise)
   }
   return {
-    promise: promise, 
-    onResolve: onResolve, 
+    promise: promise,
+    onResolve: onResolve,
     resolve: resolve
   }
 }
@@ -176,9 +176,9 @@ mongoose.Query.prototype.exec = function (op, cb) {
   var __fillsSequence = this.__fillsSequence || []
   var query = this
 
-  if (query.model.__fill && this._fields){
-    Object.keys(this._fields).forEach(function(f){
-      if (query._fields[f] == 1 && query.model.__fill[f]){
+  if (query.model.__fill && this._fields) {
+    Object.keys(this._fields).forEach(function (f) {
+      if (query._fields[f] == 1 && query.model.__fill[f]) {
         addFills(__fillsSequence, query.model, f, true)
       }
     })
@@ -187,7 +187,7 @@ mongoose.Query.prototype.exec = function (op, cb) {
   if (!__fillsSequence.length) {
     return _exec.apply(this, arguments);
   }
-  
+
   if (typeof op === 'function') {
     cb = op;
     op = null;
@@ -195,30 +195,30 @@ mongoose.Query.prototype.exec = function (op, cb) {
 
   var p = getPromise(cb)
   var promise = p.promise, onResolve = p.onResolve, resolve = p.resolve
-  
+
   _exec.call(this, op, function (err, docs) {
 
     if (err || !docs) {
       resolve(err, docs)
     } else {
 
-      async.mapSeries(__fillsSequence, function(__fills, cb){
+      async.mapSeries(__fillsSequence, function (__fills, cb) {
 
-        async.map(__fills, function(__fill, cb){
-          var useMultiWithSingle = !util.isArray(docs) && !__fill.fill.value && __fill.fill.multi
+        async.map(__fills, function (__fill, cb) {
+          var useMultiWithSingle = !Array.isArray(docs) && !__fill.fill.value && __fill.fill.multi
 
-          if (useMultiWithSingle){
+          if (useMultiWithSingle) {
             docs = [docs]
           }
 
           // TODO: make this also if there is only multi methods when one doc
-          if (util.isArray(docs)){
+          if (Array.isArray((docs))) {
             var args = getArgsWithOptions(__fill)
 
-            if (__fill.fill.multi && !__fill.fillEach){
+            if (__fill.fill.multi && !__fill.fillEach) {
 
               var index = {},
-                ids = docs.map(function(doc){
+                ids = docs.map(function (doc) {
                   var id = doc._id.toString()
                   index[id] = doc
                   return doc._id
@@ -228,41 +228,41 @@ mongoose.Query.prototype.exec = function (op, cb) {
               var multipleProps = __fill.fill.props.length > 1
 
               // set default values
-              if (__fill.fill.default){
+              if (__fill.fill.default) {
 
-                __fill.fill.props.forEach(function(prop, i){
+                __fill.fill.props.forEach(function (prop, i) {
                   var defaultPropValue =
                     multipleProps && Array.isArray(__fill.fill.default)
                       ? __fill.fill.default[i]
                       : __fill.fill.default
-                    docs.forEach(function(doc){
-                      //  ensure unique values of passed objects/arrays
-                      if (typeof defaultPropValue == 'object'){
-                        doc[prop] = Object.assign(
-                          Array.isArray(defaultPropValue) ? [] : {}
-                          , defaultPropValue)
-                      } else if (typeof defaultPropValue == 'function'){
-                        doc[prop] = defaultPropValue()
-                      } else {
-                        doc[prop] = defaultPropValue
-                      }
-                    })
+                  docs.forEach(function (doc) {
+                    //  ensure unique values of passed objects/arrays
+                    if (typeof defaultPropValue == 'object') {
+                      doc[prop] = Object.assign(
+                        Array.isArray(defaultPropValue) ? [] : {}
+                        , defaultPropValue)
+                    } else if (typeof defaultPropValue == 'function') {
+                      doc[prop] = defaultPropValue()
+                    } else {
+                      doc[prop] = defaultPropValue
+                    }
+                  })
                 })
               }
 
-              args.push(function(err, results){
+              args.push(function (err, results) {
 
-                if (results && results !== docs){
+                if (results && results !== docs) {
 
                   // convert object map to array in right order
-                  if (!util.isArray(results)){
-                    results = ids.map(function(id){
+                  if (!Array.isArray(results)) {
+                    results = ids.map(function (id) {
                       return results[id]
                     })
                   }
 
-                  results.forEach(function(r, i){
-                    if (!r){
+                  results.forEach(function (r, i) {
+                    if (!r) {
                       return
                     }
                     var doc = docs[i]
@@ -270,15 +270,15 @@ mongoose.Query.prototype.exec = function (op, cb) {
                     var spreadProps = multipleProps
 
                     // this is not the best idea, but we will allow this
-                    if (r._id && index[r._id.toString()]){
+                    if (r._id && index[r._id.toString()]) {
                       spreadProps = true
                       doc = index[r._id.toString()]
                     }
 
-                    if (!doc){return}
+                    if (!doc) { return }
 
-                    if (spreadProps){
-                      __fill.props.forEach(function(prop){
+                    if (spreadProps) {
+                      __fill.props.forEach(function (prop) {
                         doc[prop] = r[prop]
                       })
                     } else {
@@ -287,7 +287,7 @@ mongoose.Query.prototype.exec = function (op, cb) {
                     }
                   })
                 }
-                if (useMultiWithSingle){
+                if (useMultiWithSingle) {
                   cb(err, docs[0])
                 } else {
                   cb(err, docs)
@@ -297,17 +297,17 @@ mongoose.Query.prototype.exec = function (op, cb) {
               __fill.fill.multi.apply(__fill.fill, args)
 
             } else {
-              async.map(docs, function(doc, cb){
+              async.map(docs, function (doc, cb) {
                 fillDoc(doc, __fill, cb)
               }, cb)
             }
           } else {
             fillDoc(docs, __fill, cb)
           }
-        }, function(err){
+        }, function (err) {
           cb(err)
         })
-      }, function(err){
+      }, function (err) {
         resolve(err, docs)
       })
     }
@@ -316,19 +316,19 @@ mongoose.Query.prototype.exec = function (op, cb) {
   return promise
 }
 
-mongoose.Schema.prototype.fill = function(props, def) {
+mongoose.Schema.prototype.fill = function (props, def) {
 
   this.statics.__fill = this.statics.__fill || {}
 
   // return
-  if (this.statics.__fill[props]){
+  if (this.statics.__fill[props]) {
     return this.statics.__fill[props]
   }
 
   def = def || {}
 
   if (typeof def == 'function') {
-    def = {value: def}
+    def = { value: def }
   }
 
   var self = this
@@ -350,14 +350,14 @@ mongoose.Schema.prototype.fill = function(props, def) {
   var defFiller = [
     'value', 'multi',
     'query', 'debug', 'default', 'options']
-    .reduce(function(defFiller, method){
-      if (method == 'options'){
-        defFiller[method] = function(){
+    .reduce(function (defFiller, method) {
+      if (method == 'options') {
+        defFiller[method] = function () {
           def[method] = Array.prototype.slice.call(arguments);
           return defFiller
         }
       } else {
-        defFiller[method] = function(val){
+        defFiller[method] = function (val) {
           def[method] = val
           return defFiller
         }
@@ -372,7 +372,7 @@ mongoose.Schema.prototype.fill = function(props, def) {
 }
 
 
-mongoose.Query.prototype.fill = function() {
+mongoose.Query.prototype.fill = function () {
   var query = this;
   var Model = this.model;
 
@@ -387,20 +387,20 @@ mongoose.Query.prototype.fill = function() {
 }
 
 // force single fill
-mongoose.Query.prototype.fillEach = function() {
+mongoose.Query.prototype.fillEach = function () {
 
   var prevLen = this.__fills ? query.__fills.length : 0
 
   this.fill.apply(this, arguments)
 
-  for (var i = this.__fills.length - 1; i >= prevLen; i-- ){
+  for (var i = this.__fills.length - 1; i >= prevLen; i--) {
     this.__fills[i].fillEach = true
   }
 
   return this
 }
 
-function prototypeFill (doc, Model) {
+function prototypeFill(doc, Model) {
   return function () {
     var args = Array.prototype.slice.call(arguments);
     var props = args.shift()
@@ -415,7 +415,7 @@ function prototypeFill (doc, Model) {
     let p = getPromise(cb)
 
     async.mapSeries(__fillsSequence, (__fills, cb) => {
-      async.map(__fills, function(__fill, cb){
+      async.map(__fills, function (__fill, cb) {
         fillDoc(doc, __fill, cb)
       }, cb)
     }, (err) => {
@@ -425,26 +425,26 @@ function prototypeFill (doc, Model) {
     return p.promise
   }
 }
-mongoose.Model.prototype.fill = function(){
+mongoose.Model.prototype.fill = function () {
   return prototypeFill(this, this.constructor).apply(this, arguments)
 }
 
-mongoose.Types.Embedded.prototype.fill = function(){
+mongoose.Types.Embedded.prototype.fill = function () {
   return prototypeFill(this, this.schema.statics).apply(this, arguments)
 }
 
 mongoose.Types.Embedded.prototype.filled =
-mongoose.Model.prototype.filled = function(){
+  mongoose.Model.prototype.filled = function () {
 
-  var args = Array.prototype.slice.call(arguments);
-  var cb = args[args.length - 1]
-  if (typeof cb == 'function'){
-    args[args.length - 1] = function(err, doc){
-      cb(err, doc[prop])
+    var args = Array.prototype.slice.call(arguments);
+    var cb = args[args.length - 1]
+    if (typeof cb == 'function') {
+      args[args.length - 1] = function (err, doc) {
+        cb(err, doc[prop])
+      }
     }
-  }
 
-  this.fill.apply(this, args)
-}
+    this.fill.apply(this, args)
+  }
 
 module.exports = mongoose
